11dd187fdeb74b52407090b73f3be4c6aa1d9f35
naotomanzai-cpu <naotomanzai@gmail.com>
2026-04-29 09:25:09 +0900

    サービスワーカーを完全削除（端末間バグ解消）

--- sw.js
+++ sw.js
@@ -1,37 +1,10 @@
-const CACHE = 'singulink-v3';
-const ASSETS = [
-  './index.html',
-  './post.html',
-  './messages.html',
-  './profile.html',
-  './style.css',
-  './app.js',
-  './icon.svg'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
+// サービスワーカーを自己削除（過去にインストールされたSWをアンインストール）
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-// ネットワーク優先：常に最新版を取得し、失敗時のみキャッシュを使う
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        const clone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-        return res;
-      })
-      .catch(() => caches.match(e.request))
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.registration.unregister())
+      .then(() => self.clients.matchAll())
+      .then(clients => clients.forEach(c => c.navigate(c.url)))
   );
 });
